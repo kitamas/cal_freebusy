49e088586a63d657f35640d2671cea2f5069471b
kitamas <kitamas25@gmail.com>
2022-11-08 18:03:46 +0100

    commit

--- freebusy.py
+++ freebusy.py
@@ -63,8 +63,9 @@ def main():
 
     #req_data = '{"data":"Ki nyer ma","topk:":"0","temp":".1"}'
     data = {"data":"Ki nyer ma","topk:":"0","temp":".1"}
-    #data['data'] = "Milyen nap van ma"
-    data.update({'data':'Milyen nap van ma'})
+    data['data'] = "Milyen nap van ma"
+    data = str(data)
+    #data.update({'data':'Milyen nap van ma'})
 
     response = requests.post('https://polka.nytud.hu/tcom/gpt3/', headers=headers, cookies=cookies, data=data)
     print(response.json())
