4847cc308ebcaedcb369a6bdd24304484e49d899
kitamas <kitamas25@gmail.com>
2022-11-09 11:17:44 +0100

    commit

--- freebusy.py
+++ freebusy.py
@@ -66,7 +66,7 @@ def main():
 
     # data = '{"data":"Ki nyer ma","topk:":"0","temp":".1"}'
     # data = '{"data":"' + gpt3 + '","topk:":"0","temp":".1"}'
-    #data = '{"data":"' + gpt3 + '","topk:":"0","temp":"' + temp_str + '"}'
+    data = '{"data":"' + gpt3 + '","topk:":"0","temp":"' + temp_str + '"}'
     data = data.encode('utf-8')
 
     response = requests.post('https://polka.nytud.hu/tcom/gpt3/', headers=headers, cookies=cookies, data=data)
