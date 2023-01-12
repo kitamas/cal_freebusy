0d180fee65647da5ce12ba1aa724dd68a5588f73
kitamas <kitamas25@gmail.com>
2023-01-12 16:31:46 +0100

    commit

--- freebusy.py
+++ freebusy.py
@@ -35,35 +35,36 @@ def webhook():
 
     text = "*1 markdown text*, **2 markdown text**, 3 markdown text  \n"
 
-    res = {
+    res = 
+    {
        "fulfillment_response":{
           "messages":[
              {
                 "text":{
                    "text":[
-                      text
+                      "text"
                    ]
                 }
              },
-			 {
- "payload": {
- "richContent": [
- [
- {
- "type": "info",
- "title": "Info item title, card clickable tts.mp3",
- "subtitle": "Info item subtitle, robot.mp3",
- "image": {
- "src": {
- "rawUrl": "https://elearning.dev.unideb.hu/moodle37/webservice/moodlebot/robot_icon.png"
- }
- },
- "actionLink": "https://elearning.dev.unideb.hu/moodle37/webservice/moodlebot/tts.mp3"
- }
- ]
- ]
- }
- },
+             {
+                "payload":{
+                   "richContent":[
+                      [
+                         {
+                            "type":"info",
+                            "title":"Info item title, card clickable tts.mp3",
+                            "subtitle":"Info item subtitle, robot.mp3",
+                            "image":{
+                               "src":{
+                                  "rawUrl":"https://elearning.dev.unideb.hu/moodle37/webservice/moodlebot/robot_icon.png"
+                               }
+                            },
+                            "actionLink":"https://elearning.dev.unideb.hu/moodle37/webservice/moodlebot/tts.mp3"
+                         }
+                      ]
+                   ]
+                }
+             },
              {
                 "payload":{
                    "richContent":[
@@ -72,22 +73,22 @@ def webhook():
                             "type":"chips",
                             "options":[
                                {
-                                  "text":"Chips 1",
+                                  "text":"AI csapat",
                                   "image":{
                                      "src":{
-                                        "rawUrl":"https://example.com/images/logo.png"
+                                        "rawUrl":"https://dev.da.tsmcloud.hu/var/www/images/icon1.png"
                                      }
                                   },
-                                  "link":"https://example.com"
+                                  "link":"https://dev.da.tsmcloud.hu/"
                                },
                                {
-                                  "text":"Chips 2",
+                                  "text":"Telekom",
                                   "image":{
                                      "src":{
-                                        "rawUrl":"https://example.com/images/logo.png"
+                                        "rawUrl":"https://dev.da.tsmcloud.hu/var/www/images/icon2.png"
                                      }
                                   },
-                                  "link":"https://example.com"
+                                  "link":"https://www.telekom.hu/"
                                }
                             ]
                          }
@@ -95,184 +96,186 @@ def webhook():
                    ]
                 }
              },
-			  {
- "payload": {
- "richContent": [
-  [
-  {
-  "title": "Description title",
-  "type": "description",
-  "text": [
-  "This is text line 1",
-  "This is text line 2"
-  ]
-  }
-  ]
- ]
- }
- },
-{
- "payload": {
- "richContent": [
-  [
-  {
-  "type": "image",
-  "accessibilityText": "image",
-  "rawUrl": "https://www.creativefabrica.com/wp-content/uploads/2019/05/Robot-icon-by-ahlangraphic-580x386.jpg"
-  }
-  ]
- ]
- }
- },
-{
- "payload": {
- "richContent": [
-  [
-  {
-  "link": "https://elearning.dev.unideb.hu/moodle37/webservice/moodlebot/robot.mp3",
-  "text": "BUTTON link /robot.mp3",
-  "icon": {
-  "type": "link",
-  "color": "#FF9800"
-  },
-  "type": "button"
-  }
-  ]
- ]
- }
- },
- {
- "payload": {
- "richContent": [
-  [
- {
- "type": "button",
- "icon": {
-  "type": "chevron_right",
-  "color": "#FF9800"
- },
- "text": "Button event, parameter",
- "link": "https://",
- "event": {
-  "name": "",
-  "languageCode": "",
-  "parameters": {"param1":"1"}
- }
- }
-  ]
- ]
- }
- },
- {
- "payload": {
- "richContent": [
-  [
-  {
-  "type": "chips",
-  "options": [
-  {
-   "text": "robot",
-   "image": {
-   "src": {
-   "rawUrl": "https://elearning.dev.unideb.hu/moodle37/webservice/moodlebot/robot_icon.png"
-   }
-   },
-   "link": "https://elearning.dev.unideb.hu/moodle37/webservice/moodlebot/robot.mp3"
-  },
-  {
-   "text": "yes",
-   "image": {
-   "src": {
-   "rawUrl": "https://elearning.dev.unideb.hu/moodle37/webservice/moodlebot/yes.png"
-   }
-   },
-   "link": "https://cloud.google.com/dialogflow/case-studies"
-  },
-  {
-   "text": "no",
-   "image": {
-   "src": {
-   "rawUrl": "https://elearning.dev.unideb.hu/moodle37/webservice/moodlebot/no.png"
-   }
-   },
-   "link": "https://cloud.google.com/dialogflow/docs"
-  }
-  ]
-  }
-  ]
- ]
- }
- },
-
- {
- "payload": {
- "richContent": [
-  [
-  {
-  "type": "chips",
-  "options": [
-  {
-   "text": "Start over"
-  }
-  ]
-  }
-  ]
- ]
- }
- },
-
-{
- "payload": {
- "richContent": [
-  [
-  {
-  "subtitle": "list item 1 subtitle",
-  "title": "list item 1 title",
-  "type": "list",
-  "event": {
-  "parameters": {
-   "param2": "prm2"
-  },
-  "name": "WELCOME",
-  "languageCode": "en"
-  }
-  },
-  {
-  "type": "divider"
-  },
-  {
-  "subtitle": "list item 2 subtitle",
-  "title": "list item 2 title",
-  "type": "list",
-  "event": {
-  "parameters": {},
-  "name": "PARALELL",
-  "languageCode": "en"
-  }
-  }
-  ]
- ]
- }
- },
-{
- "payload": {
- "richContent": [
-  [
-  {
-  "subtitle": "accordion subtitle",
-  "type": "accordion",
-  "title": "accordion title",
-  "text": "Lorem <img src=\"https://www.creativefabrica.com/wp-content/uploads/2019/05/Robot-icon-by-ahlangraphic-580x386.jpg\" width=\"38\"><a target=\"_blank\" href=\"https://moodle.org\">Moodle link</a> ipsum dolor sit amet.",
-  "image": {
-  "src": {
-   "rawUrl": "https://www.creativefabrica.com/wp-content/uploads/2019/05/Robot-icon-by-ahlangraphic-580x386.jpg"
-  }
-  }
-  }
-  ]
- ]
- }
- }
+             {
+                "payload":{
+                   "richContent":[
+                      [
+                         {
+                            "title":"Description title",
+                            "type":"description",
+                            "text":[
+                               "This is text line 1",
+                               "This is text line 2"
+                            ]
+                         }
+                      ]
+                   ]
+                }
+             },
+             {
+                "payload":{
+                   "richContent":[
+                      [
+                         {
+                            "type":"image",
+                            "accessibilityText":"image",
+                            "rawUrl":"https://www.creativefabrica.com/wp-content/uploads/2019/05/Robot-icon-by-ahlangraphic-580x386.jpg"
+                         }
+                      ]
+                   ]
+                }
+             },
+             {
+                "payload":{
+                   "richContent":[
+                      [
+                         {
+                            "link":"https://elearning.dev.unideb.hu/moodle37/webservice/moodlebot/robot.mp3",
+                            "text":"BUTTON link /robot.mp3",
+                            "icon":{
+                               "type":"link",
+                               "color":"#FF9800"
+                            },
+                            "type":"button"
+                         }
+                      ]
+                   ]
+                }
+             },
+             {
+                "payload":{
+                   "richContent":[
+                      [
+                         {
+                            "type":"button",
+                            "icon":{
+                               "type":"chevron_right",
+                               "color":"#FF9800"
+                            },
+                            "text":"Button event, parameter",
+                            "link":"https://",
+                            "event":{
+                               "name":"",
+                               "languageCode":"",
+                               "parameters":{
+                                  "param1":"1"
+                               }
+                            }
+                         }
+                      ]
+                   ]
+                }
+             },
+             {
+                "payload":{
+                   "richContent":[
+                      [
+                         {
+                            "type":"chips",
+                            "options":[
+                               {
+                                  "text":"robot",
+                                  "image":{
+                                     "src":{
+                                        "rawUrl":"https://elearning.dev.unideb.hu/moodle37/webservice/moodlebot/robot_icon.png"
+                                     }
+                                  },
+                                  "link":"https://elearning.dev.unideb.hu/moodle37/webservice/moodlebot/robot.mp3"
+                               },
+                               {
+                                  "text":"yes",
+                                  "image":{
+                                     "src":{
+                                        "rawUrl":"https://elearning.dev.unideb.hu/moodle37/webservice/moodlebot/yes.png"
+                                     }
+                                  },
+                                  "link":"https://cloud.google.com/dialogflow/case-studies"
+                               },
+                               {
+                                  "text":"no",
+                                  "image":{
+                                     "src":{
+                                        "rawUrl":"https://elearning.dev.unideb.hu/moodle37/webservice/moodlebot/no.png"
+                                     }
+                                  },
+                                  "link":"https://cloud.google.com/dialogflow/docs"
+                               }
+                            ]
+                         }
+                      ]
+                   ]
+                }
+             },
+             {
+                "payload":{
+                   "richContent":[
+                      [
+                         {
+                            "type":"chips",
+                            "options":[
+                               {
+                                  "text":"Start over"
+                               }
+                            ]
+                         }
+                      ]
+                   ]
+                }
+             },
+             {
+                "payload":{
+                   "richContent":[
+                      [
+                         {
+                            "subtitle":"list item 1 subtitle",
+                            "title":"list item 1 title",
+                            "type":"list",
+                            "event":{
+                               "parameters":{
+                                  "param2":"prm2"
+                               },
+                               "name":"WELCOME",
+                               "languageCode":"en"
+                            }
+                         },
+                         {
+                            "type":"divider"
+                         },
+                         {
+                            "subtitle":"list item 2 subtitle",
+                            "title":"list item 2 title",
+                            "type":"list",
+                            "event":{
+                               "parameters":{
+                                  
+                               },
+                               "name":"PARALELL",
+                               "languageCode":"en"
+                            }
+                         }
+                      ]
+                   ]
+                }
+             },
+             {
+                "payload":{
+                   "richContent":[
+                      [
+                         {
+                            "subtitle":"accordion subtitle",
+                            "type":"accordion",
+                            "title":"accordion title",
+                            "text":"Lorem <img src=\"https://www.creativefabrica.com/wp-content/uploads/2019/05/Robot-icon-by-ahlangraphic-580x386.jpg\" width=\"38\"><a target=\"_blank\" href=\"https://moodle.org\">Moodle link</a> ipsum dolor sit amet.",
+                            "image":{
+                               "src":{
+                                  "rawUrl":"https://www.creativefabrica.com/wp-content/uploads/2019/05/Robot-icon-by-ahlangraphic-580x386.jpg"
+                               }
+                            }
+                         }
+                      ]
+                   ]
+                }
+             }
           ]
        }
     }
