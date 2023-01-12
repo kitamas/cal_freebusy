278712e003d213e2ca65b1841acf715a38fbd077
kitamas <kitamas25@gmail.com>
2023-01-12 12:58:10 +0100

    commit

--- freebusy.py
+++ freebusy.py
@@ -30,99 +30,64 @@ def favicon():
                                'favicon.ico', mimetype='image/favicon.png')
 
 
-@app.route('/authentication')
-
-def authentication():
-    creds = google_oauth2_service_account.Credentials.from_service_account_info(
-    {
-  "type": "service_account",
-  "project_id": "my-project-90818-learn-hun",
-  "private_key_id": "cf8bd4105633c3cac528d1c8c4c66cc3b825e837",
-  "private_key": "-----BEGIN PRIVATE KEY-----\nMIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQCeQrDkS5Rq/khX\nNoU01bLEbZHj/JjzwlcozMB0uRP+H9NYJytYrl4+vqhi3+EApY+bKpU/iB/A9tPC\nVIYjhXeACCXtNq6sz8hQDN6y+cKW5K4i2yd1xpeIlAXjo4M/uRIarlffgEi3MOAX\nRtTDskXvECuu2rXsIbgQEBzpCwJQhaVWnOHk4ONTJnEKSjvtuHXRZUc6GrabLrLU\nb4h1nDYh8+IVoUxpcZg0sW9XWpXw2O3kh8o6ADy4aH2KB9Sv4yktLLNcVWrqisei\nekA93NnWji5pG2DKuqjrmaZGA9SwOA5gT34EfrbL8zVFaEGgV7yp5ytrTg5vmtaw\nP36j58iJAgMBAAECggEAG5iV900NjYWXECwX2LFtuW5AuwBEHHc2Ew12/rN6Hr0m\ncW/tEUrgcLD2tD0FI0N7UcuAWGJwZQm1PaTW+hEvGAJzuJQpK8WUkI7Z81v1WDH6\ngmX0EMenC0ACceIEhCNNmpztgjHAnD73yF9HwPMQWkIX1+bXw5vSmGxy2hkbF3ac\nKehAlVB584QC8L191WtWPpx27pdNy2ncnlW6Xaail8NtGlCELLGrmicQUjgxZhsU\ngEZh/ZrTYm4ion5mWCYtbSvuXN++9Zy6kIdpYiFOroQuEr48L6l0KLmVvNsuZ1D0\nk4xdXJxtx0kacGr0tyg1VV3vut14vcSlDqbeDUy7DQKBgQC/5LuQVTJT3FiJ13sb\nCyJIuFVQAX2p1+TXsiKJh9Copaua0tvXygJtbGFEVmYKS3VwiC8OyESQQhATFORE\nYZYpek0UwhNggekg+asiMMSnPc/hHkxQdsxmT34sfmuooSeDvqrVPw6BW3i7Hb6Q\nB7lfydH+v6VejPKWm2q7sm6UpQKBgQDTIZJ/rdgknqnIENgcHwRJoKS3j2RcCvnK\nu0pXoumA9xq1/JFOCuGmRnscyga0d17fYGpER3iR5NL2YAksMSUEDyj+yFupHyfS\nTZzVVaeYNW3OKjDXwUDGYh2D3JkF7P6zeC9LLBkvwuhllvwC1URewWARBpreVGpH\nuZGRo6CLFQKBgHnO9yTif+TtzSIKr3F2OtgQcs8rcxpaGkC1KelFVjWHnIvV54lu\nCOZu0rtvYKyOQ8kgGUb351XvKYcDTvb9PzWrFbzkiSpMrLCq62/zpxFGUmvjMKwv\nDQaw1TXnNe3ABnZBlO1yboG8j8GvWuTQkmJ0mSFtg8qmC+OAWls1I66lAoGAZCvL\njBR5Nnao6ylCv6Tfrecv/39jCGCUv2E5FndO/kc/PxUEA9kZ0oAiLTiVEc6JDsZ5\n5MdcJyxAA3DxKSxv+YsP0kJRat5DUH5OaNFo4MiIvoY6AkPIbddjVYq2d59IAPKG\nzc2wbX62MG0ASH/THnn1EF7n35CBlGIw9L6DjzkCgYEAu0LhdTHuuM6DOAfh4ohw\n0oj2770Dyw0vQImp+4qbdocLqXIVOZYEIywGfi+OLuSxrSYsw2BuTAPAtIEmkuHx\n4cvV/XxcWWPY30B4Z2irOBIsIXJjfAO/DyCyDaX7Cmoy+NgoCwRSRzDeOiQ7ysw8\nhwrS/OoTx3BhHYJt+NO6qIM=\n-----END PRIVATE KEY-----\n",
-  "client_email": "appointment-scheduler@my-project-90818-learn-hun.iam.gserviceaccount.com",
-  "client_id": "115405775326578876255",
-  "auth_uri": "https://accounts.google.com/o/oauth2/auth",
-  "token_uri": "https://oauth2.googleapis.com/token",
-  "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
-  "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/appointment-scheduler%40my-project-90818-learn-hun.iam.gserviceaccount.com"
-    },
-    scopes=['https://www.googleapis.com/auth/calendar'],
-    subject='appointment-scheduler@my-project-90818-learn-hun.iam.gserviceaccount.com'
-    )
-    return creds
-
-@app.route('/webhook', methods=['GET','POST'])
+@ app.route('/webhook', methods = ['GET', 'POST'])
 def webhook():
 
     text = "*1 markdown text*, **2 markdown text**, 3 markdown text  \n"
 
-    res = {
-    "fulfillment_response":{
-      "messages":[
-         {
+res = {
+    "fulfillment_response": {
+        "messages": [{
 
-            "text":{
-               :[
-                  "Please select anyone"
-               ]
+            "text": {: [
+                    "Please select anyone"
+                ]
             },
-            "payload":{
-               "richContent":[
-                  [
-                     {
-                        "type":"chips",
-                        "options":[
-                           {
-                              "text":"Chip 1",
-                              "image":{
-                                 "src":{
-                                    "rawUrl":"https://example.com/images/logo.png"
-                                 }
-                              },
-                              "link":"https://example.com"
-                           },
-                           {
-                              "text":"Chip 2",
-                              "image":{
-                                 "src":{
-                                    "rawUrl":"https://example.com/images/logo.png"
-                                 }
-                              },
-                              "link":"https://example.com"
-                           }
-                        ]
-                     }
-                  ]
-               ]
+            "payload": {
+                "richContent": [
+                    [{
+                        "type": "chips",
+                        "options": [{
+                            "text": "Chip 1",
+                            "image": {
+                                "src": {
+                                    "rawUrl": "https://example.com/images/logo.png"
+                                }
+                            },
+                            "link": "https://example.com"
+                        }, {
+                            "text": "Chip 2",
+                            "image": {
+                                "src": {
+                                    "rawUrl": "https://example.com/images/logo.png"
+                                }
+                            },
+                            "link": "https://example.com"
+                        }]
+                    }]
+                ]
             }
-         }
-      ]
+        }]
     }
-    }
+}
 
-	
+
 """
-    res = {
-        "fulfillment_response": {
-            "messages": [
-                {
-                    "text": {
-                        "text": [
-                            text
-                        ]
-                    }
-                }
-            ]
-        },
-        "session_info": {
-            "session" : "session_name",
-            "parameters": {
-                "event_id" : "event_id"
+res = {
+    "fulfillment_response": {
+        "messages": [{
+            "text": {
+                "text": [
+                    text
+                ]
             }
+        }]
+    },
+    "session_info": {
+        "session": "session_name",
+        "parameters": {
+            "event_id": "event_id"
         }
     }
+}
 """
-
-    return res
-
-# app.run()
