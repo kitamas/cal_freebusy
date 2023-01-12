172851b0670a1c0c34602f55100cf2fda435305a
kitamas <kitamas25@gmail.com>
2023-01-12 13:21:48 +0100

    commit

--- freebusy.py
+++ freebusy.py
@@ -36,44 +36,46 @@ def webhook():
     text = "*1 markdown text*, **2 markdown text**, 3 markdown text  \n"
 
     res = {
-        "fulfillment_response": {
-            "messages": [{
-                "text": {
-                    "text": [
-                        text
-                    ]
-        },
-           "payload":{
-               "richContent":[
-                  [
-                     {
-                        "type":"chips",
-                        "options":[
-                           {
-                              "text":"Chip 1",
-                              "image":{
-                                 "src":{
-                                    "rawUrl":"https://example.com/images/logo.png"
-                                 }
-                              },
-                              "link":"https://example.com"
-                           },
-                           {
-                              "text":"Chip 2",
-                              "image":{
-                                 "src":{
-                                    "rawUrl":"https://example.com/images/logo.png"
-                                 }
-                              },
-                              "link":"https://example.com"
-                           }
-                        ]
-                     }
-                  ]
-               ]
-            }
-         }
-      ]
+       "fulfillment_response":{
+          "messages":[
+             {
+                "text":{
+                   "text":[
+                      "text"
+                   ]
+                },
+                "payload":{
+                   "richContent":[
+                      [
+                         {
+                            "type":"chips",
+                            "options":[
+                               {
+                                  "text":"Chip 1",
+                                  "image":{
+                                     "src":{
+                                        "rawUrl":"https://example.com/images/logo.png"
+                                     }
+                                  },
+                                  "link":"https://example.com"
+                               },
+                               {
+                                  "text":"Chip 2",
+                                  "image":{
+                                     "src":{
+                                        "rawUrl":"https://example.com/images/logo.png"
+                                     }
+                                  },
+                                  "link":"https://example.com"
+                               }
+                            ]
+                         }
+                      ]
+                   ]
+                }
+             }
+          ]
+       }
     }
-    }
+
     return res
