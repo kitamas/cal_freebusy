393e94cd10234ff77643acb6243a77a7348294f9
kitamas <kitamas25@gmail.com>
2023-01-16 11:55:28 +0100

    commit

--- freebusy.py
+++ freebusy.py
@@ -34,7 +34,6 @@ def favicon():
 def webhook():
 
     text = " *bold*  \n **mdw** _italic_ [link](https://facebook.com) "
-
     res = {
        "fulfillment_response":{
           "messages":[
@@ -47,68 +46,64 @@ def webhook():
              },
              {
                 "payload":{
-                       "attachment":{
+                  "attachment":{
       "type":"template",
       "payload":{
-        "template_type":"receipt",
-        "recipient_name":"Stephane Crozatier",
-        "order_number":"12345678902",
-        "currency":"USD",
-        "payment_method":"Visa 2345",        
-        "order_url":"http://originalcoastclothing.com/order?order_id=123456",
-        "timestamp":"1428444852",         
-        "address":{
-          "street_1":"1 Hacker Way",
-          "street_2":"",
-          "city":"Menlo Park",
-          "postal_code":"94025",
-          "state":"CA",
-          "country":"US"
-        },
-        "summary":{
-          "subtotal":75.00,
-          "shipping_cost":4.95,
-          "total_tax":6.19,
-          "total_cost":56.14
-        },
-        "adjustments":[
-          {
-            "name":"New Customer Discount",
-            "amount":20
-          },
-          {
-            "name":"$10 Off Coupon",
-            "amount":10
-          }
-        ],
+        "template_type":"generic",
         "elements":[
-          {
-            "title":"Classic White T-Shirt",
-            "subtitle":"100% Soft and Luxurious Cotton",
-            "quantity":2,
-            "price":50,
-            "currency":"USD",
-            "image_url":"http://originalcoastclothing.com/img/whiteshirt.png"
+           {
+            "title":"Welcome!",
+            "image_url":"https://www.creativefabrica.com/wp-content/uploads/2019/05/Robot-icon-by-ahlangraphic-580x386.jpg",
+            "subtitle":"We have the right hat for everyone.",
+            "default_action": {
+              "type": "web_url",
+              "url": "https://www.originalcoastclothing.com/",
+              "webview_height_ratio": "tall"
+            },
+            "buttons":[
+              {
+                "type":"web_url",
+                "url":"https://www.originalcoastclothing.com/",
+                "title":"View Website"
+              },
+			  {
+                "type":"postback",
+                "title":"Start Chatting",
+				"payload":"PAYLOAD"
+              }              
+            ]      
           },
-          {
-            "title":"Classic Gray T-Shirt",
-            "subtitle":"100% Soft and Luxurious Cotton",
-            "quantity":1,
-            "price":25,
-            "currency":"USD",
-            "image_url":"http://originalcoastclothing.com/img/grayshirt.png"
+		             {
+            "title":"Welcome!",
+            "image_url":"https://www.creativefabrica.com/wp-content/uploads/2019/05/Robot-icon-by-ahlangraphic-580x386.jpg",
+            "subtitle":"We have the right hat for everyone.",
+            "default_action": {
+              "type": "web_url",
+              "url": "https://www.originalcoastclothing.com/",
+              "webview_height_ratio": "tall"
+            },
+            "buttons":[
+              {
+                "type":"web_url",
+                "url":"https://www.originalcoastclothing.com/",
+                "title":"View Website"
+              },
+			  {
+                "type":"postback",
+                "title":"Start Chatting",
+				"payload":"PAYLOAD"
+              }              
+            ]      
           }
         ]
       }
-    }
+    }     
              }
           ]
        }
     } 
 
-
-
-     
+         
     
     """
     res = {
