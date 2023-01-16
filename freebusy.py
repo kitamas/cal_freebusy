ad681d4705729bc4c5efefea431f65da12d1bd08
kitamas <kitamas25@gmail.com>
2023-01-16 12:32:18 +0100

    commit

--- freebusy.py
+++ freebusy.py
@@ -47,56 +47,51 @@ def webhook():
              },
              {
                 "payload":{
-        "attachment":
-        {
-            "type": "template",
-            "payload": {
-                "template_type": "generic",
-                "elements": [
-                    {
-                        "title": "Title",
-                         "subtitle": "subtitle",
-                        "image_url": "url of the image",
-                        "buttons": [
+                   "attachment":{
+                      "type":"template",
+                      "payload":{
+                         "template_type":"generic",
+                         "elements":[
                             {
-                                "title": "visit",
-                                "url": "webpage url to open",
-                                "type": "web_url"
-                            }
-                        ]
-                    },
-                    {
-                        "title": "Title",
-                        "subtitle": "subtitle",
-                        "image_url": "url of the image",
-                        "buttons": [
+                               "title":"Welcome!",
+                               "image_url":"https://www.creativefabrica.com/wp-content/uploads/2019/05/Robot-icon-by-ahlangraphic-580x386.jpg",
+                               "subtitle":"We have the right hat for everyone.",
+                               "default_action":{
+                                  "type":"web_url",
+                                  "url":"https://www.originalcoastclothing.com/",
+                                  "webview_height_ratio":"tall"
+                               },
+                               "buttons":[
+                                  {
+                                     "type":"web_url",
+                                     "url":"https://www.originalcoastclothing.com/",
+                                     "title":"View Website"
+                                  },
+                                  {
+                                     "type":"postback",
+                                     "title":"Start Chatting",
+                                     "payload":"PAYLOAD"
+                                  }
+                               ]
+                            },
                             {
-                                "title": "Title",
-                                "url": "webpage url to open",
-                                "type": "web_url"
-                            }
-                        ]
-                    },
-                    {
-                        "title": "Title",
-                        "subtitle": "subtitle",
-                        "image_url": "url of the image",
-                        "buttons": [
-                            {
-                                "title": "Title",
-                                "url": "webpage url to open",
-                                "type": "web_url"
-                            }
-                        ]
-                    }
-                ]
-            }
-        }
-    }
+                               "title":"Welcome!",
+                               "image_url":"https://www.creativefabrica.com/wp-content/uploads/2019/05/Robot-icon-by-ahlangraphic-580x386.jpg",
+                               "subtitle":"We have the right hat for everyone.",
+                               "default_action":{
+                                  "type":"web_url",
+                                  "url":"https://www.originalcoastclothing.com/",
+                                  "webview_height_ratio":"tall"
+                               }
+                         ]
+                      }
+                   }
+                }
              }
           ]
        }
-    } 
+    }
+
 
 
     """
