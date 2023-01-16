62222d2005b74fa6ac7caf5b837426455b3ea8fc
kitamas <kitamas25@gmail.com>
2023-01-16 12:34:26 +0100

    commit

--- freebusy.py
+++ freebusy.py
@@ -73,16 +73,7 @@ def webhook():
                                      "payload":"PAYLOAD"
                                   }
                                ]
-                            },
-                            {
-                               "title":"Welcome!",
-                               "image_url":"https://www.creativefabrica.com/wp-content/uploads/2019/05/Robot-icon-by-ahlangraphic-580x386.jpg",
-                               "subtitle":"We have the right hat for everyone.",
-                               "default_action":{
-                                  "type":"web_url",
-                                  "url":"https://www.originalcoastclothing.com/",
-                                  "webview_height_ratio":"tall"
-                               }
+                            }
                          ]
                       }
                    }
